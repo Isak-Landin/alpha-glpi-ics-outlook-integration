d86f6880e111d1424e04c7ffa2654c649425af72
Isak-Landin <utbisalan@gmail.com>
2025-03-31 14:55:46 +0200

    Removed sensitive data (tokens)

--- dependencies/tokent_auth_direct_download.ps1
+++ dependencies/tokent_auth_direct_download.ps1
@@ -1,13 +1,8 @@
 ﻿# Load User Configurations
 
 # Comparison URL
-# $icsUrl = "https://support.compliq.se/front/planning.php?genical=1&uID=306&gID=0&entities_id=0&is_recursive=1&token=0sG8nv7V0owVbBlWwx5tqPUmT3Blu0PEZoqQPZbz"
-# $icsUrl = "https://support.compliq.se/front/planning.php?genical=1&uID=306&gID=0&entities_id=0&is_recursive=1&token=0sG8nv7V0owVbBlWwx5tqPUmT3Blu0PEZoqQPZbz"
 $icsUrl = "https://support.compliq.se/front/planning.php?genical=1&uID=8&gID=0&entities_id=0&is_recursive=1&token=sEB6oBgoq3HrPSr5IT32QDDG3CylUH4Wvahj1W1w"
 $icsUrl = null
-
-## Personal token not working, just generating a blank page
-# $icsUrl = "https://support.compliq.se/front/planning.php?genical=1&uID=306&gID=0&entities_id=0&is_recursive=1&token=ov9ZA0NpTZoRytgbWhl7IBSdX77CLTcsNDZAA1QM"
 
 Write-Host $icsUrl
 
